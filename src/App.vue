1ec65131b1eba0e7851fc017cb1ce24597fcc642
quasar1533 <1029230641@qq.com>
2021-02-12 23:45:17 +0800

    version7 学习前端路由以及VueRouter相关

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <!-- <input v-model="message" placeholder="edit me" />
-    <p>Message is: {{ message }}</p>
-    <p>
-      <button @click="message = 'halo'">set message</button>
-    </p>
-    <hr />
-    <Example1 />
-    <hr />
-    <Example2 />
-    <hr />
-    <Example3 /> -->
-    <!-- ---------------------------------------------------------------- -->
-    <Example4 />
+
   </div>
 </template>
 
@@ -21,16 +9,12 @@
 // import Example1 from "./example1.vue";
 // import Example2 from "./example2.vue";
 // import Example3 from "./example3.vue";
-import Example4 from "./example4.vue";
 
 export default {
   name: "App",
   components: {
     // HelloWorld,
     // Example1,
-    // Example2,
-    // Example3,
-    Example4,
   },
   data() {
     return {
---
+++ src/learning/index3.js
@@ -0,0 +1,59 @@
+const div1 = document.createElement("div");
+div1.innerHTML = 1;
+const div2 = document.createElement("div");
+div2.innerHTML = 2;
+const div3 = document.createElement("div");
+div3.innerHTML = 3;
+const div4 = document.createElement("div");
+div4.innerHTML = 4;
+const routeTable = {
+  "1": div1,
+  "2": div2,
+  "3": div3,
+  "4": div4,
+};
+
+function route(container) {
+  const app = document.querySelector(`#${container}`);
+  let number = window.location.pathname.substring(1);
+  if (number.includes("html")) {
+    number = 1;
+  }
+  let div = routeTable[number];
+  if (!div) {
+    div = document.querySelector("#div404");
+  }
+  div.style.display = "block";
+
+  if (app.children.length > 0) {
+    if (div !== app.children[0]) {
+      app.children[0].style.display = "none";
+    }
+    document.body.appendChild(app.children[0]);
+  }
+  app.appendChild(div);
+}
+function onPushState() {
+  console.log("history changed");
+  route("app");
+}
+
+const allATags = document.querySelectorAll("a.history");
+for (let a of allATags) {
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = a.getAttribute("href");
+    console.log(href);
+    window.history.pushState(null, `page${href}`, href);
+    onPushState();
+  });
+}
+
+route("app");
+
+window.addEventListener("hashchange", () => {
+  console.log("hash changed");
+  route("app");
+});
+
+// console.log(window.location.pathname);
